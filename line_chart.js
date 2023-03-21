c347f3b0381870b33151a6136a0c903a30acbb27
Simran Kaur <74691300+skaur42@users.noreply.github.com>
2023-03-21 00:47:58 -0400

    Create line_chart.js

---
+++ line_chart.js
@@ -0,0 +1,5 @@
+//=======================================================================================================
+// Functions to draw a Line Chart - Allows 1 data point per 25px of canvas height (e.g. 12 per 300px)
+// Must provide: dataPoints object, canvas id, legend id
+// Each data point is a name and an array of numeric values (e.g. name:[123,234,345])
+//=======================================================================================================
